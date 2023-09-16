f4fcd1df9c86e8d6faabd0607f6c9f8cc59de1b2
wayungi <fwayungi.com>
2023-09-16 11:49:04 +0300

    ADD: searchMovie method

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import Home from './components/Home';
 import data from './data.json';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 
 
 const App = () => {
 
-  const [movies, setMovies] = useState(data.Search);
-  
-  console.log(movies)
+  const [movies, setMovies] = useState([]);
+
+  const searchMovie = async () => {
+    const URL =  `http://www.omdbapi.com/?s=star wars&apikey=fa994858`;
+    const result = await fetch(URL);
+    const resultJson = await result.json();
+    setMovies(resultJson.Search);
+  }
+
+  useEffect(() => {
+    const result = searchMovie();  
+  }, []);
+
 
   return (
     <div className="app">
--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
-const searchMovie = async (serachTerm) => {
+export const searchMovie = async (searchTerm) => {
     const URL =  `http://www.omdbapi.com/?s=${searchTerm}&apikey=fa994858`;
     const result = await fetch(URL);
     const resultJson = result.json();
-    return resultJson;
+    console.log(resultJson.Search);
+    return resultJson.Search;
 }
