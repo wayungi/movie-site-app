08c5b3196aead3e0a5a1dfd538b2f7e3b05e94d7
wayungi <fwayungi.com>
2023-09-16 10:06:46 +0300

    ADD: Porject setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+
+
+const Home = () => {
+
+    const [movies, setMovies] = useState([
+        
+    ]);
+
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
