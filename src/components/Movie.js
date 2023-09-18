b37272af49c2dcfff9e9f56c42ee89c72892955f
wayungi <fwayungi.com>
2023-09-19 00:01:28 +0300

    ADD: fFavourite button component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,10 +1,10 @@
+import FavouriteButton from "./FavouriteButton";
+
 const Movie = ({movie}) => {
     return (
         <div className="movie">
             <img src={movie.Poster} alt={movie.Title}/>
-            <div className="overlay">
-                Add to Favourites
-            </div>
+            <FavouriteButton />
         </div>
     );
 };
